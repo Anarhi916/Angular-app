72d67663e7289ed777b445c8e0ddc8c32ba6d8f0
C5340927 <andriy.filimonov@sap.com>
2023-12-29 16:11:51 +0200

    enhanced create form

--- src/app/components/create-store-dialog/create-store-dialog.component.ts
+++ src/app/components/create-store-dialog/create-store-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-store-dialog',
@@ -9,11 +9,30 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class CreateStoreDialogComponent {
   constructor(public dialogRef: MatDialogRef<CreateStoreDialogComponent>) {}
-  name = new FormControl('', [Validators.required]);
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    address: new FormControl('', [Validators.required]),
+    established: new FormControl('', [Validators.required]),
+    floorArea: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    tel: new FormControl('', [
+      Validators.required,
+      Validators.minLength(13),
+      Validators.maxLength(13),
+    ]),
+  });
 
-  getErrorMessage(): any {
-    if (this.name.hasError('required')) {
+  getErrorMessage(fieldName: string): string {
+    const control = this.form.get(fieldName) as FormControl;
+    if (control.hasError('required')) {
       return 'You must enter a value';
     }
+    if (control.hasError('email')) {
+      return 'Not a valid email';
+    }
+    if (control.hasError('minlength') || control.hasError('maxlength')) {
+      return 'Number must contain 13 characters';
+    }
+    return '';
   }
 }
